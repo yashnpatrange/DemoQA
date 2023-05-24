0fc74e987511a84e91d05ab78f5eb933950e7ca6
Yash Patrange <ypatrange@qac.com>
2023-05-24 15:58:30 -0400

    Product purchase test scenario added

--- src/main/java/org/example/StepDefination/CucumberSteps.java
+++ src/main/java/org/example/StepDefination/CucumberSteps.java
@@ -6,32 +6,47 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CucumberSteps{
 
     public WebDriver driver;
 
-    @Given("the user opens the browser and lands on {string}")
+    @Given("the user opens the browser and navigates to the homepage {string}")
     public void OpenBrowser(String website) {
         driver= new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.get(website);
-
     }
     @When("the user enters {string} and {string}")
     public void EnterCredentials(String username, String password){
-        driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys(username);
-        driver.findElement(By.xpath("//input[@id='password']")).sendKeys(password);
+        WebElement uname= driver.findElement(By.xpath("//input[@id='user-name']"));
+        uname.sendKeys(username);
+        WebElement paswd=driver.findElement(By.xpath("//input[@id='password']"));
+        paswd.sendKeys(password);
     }
-
     @Then("the user should be able to login")
     public void logIn(){
         driver.findElement(By.xpath("//input[@id='login-button']")).click();
     }
-
-
+    @And("the 'Sauce Labs' logo is displayed")
+    public void theLogoIsDisplayed() {
+        driver.findElement(By.xpath("//div[@class='app_logo']")).isDisplayed();
+    }
+    @When("the user clicks on 'Add Product button'")
+    public void addProducts() {
+        WebElement Product1 = driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-backpack']"));
+        WebElement Product2= driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-bike-light']"));
+        Product1.click();
+        Product2.click();
+    }
+    @Then("the user click on cart logo")
+    public void theUserClickOnCartLogo() {
+        WebElement cartLogo= driver.findElement(By.xpath("//a[@class='shopping_cart_link']"));
+        cartLogo.click();
+    }
     @And("(the user) closes the browser")
     public void CloseBrowser(){
         driver.close();
@@ -39,10 +54,5 @@ public class CucumberSteps{
     }
 
 
-//    public void theUserOpensTheBrowserAndLandsOn(String arg0) {
-//    }
-//
-//
-//    public void theUserShouldBeAbleToLogin() {
-//    }
+
 }
--- src/test/resources/Features/test.feature
+++ src/test/resources/Features/test.feature
@@ -1,7 +1,28 @@
 Feature: Test Feature file
 
-  Scenario: Opening the Chrome browser
-    Given the user opens the browser and lands on "https://www.saucedemo.com/"
+  Scenario: Opening and CLosing the Chrome browser
+    Given the user opens the browser and navigates to the homepage "https://www.saucedemo.com/"
+    And the user closes the browser
+
+
+  Scenario: The user logs in Successfully
+    Given the user opens the browser and navigates to the homepage "https://www.saucedemo.com/"
     When the user enters "standard_user" and "secret_sauce"
     Then the user should be able to login
-    And the user closes the browser
+    And the 'Sauce Labs' logo is displayed
+    Then the user closes the browser
+    
+  Scenario: Add products to the cart
+    Given the user opens the browser and navigates to the homepage "https://www.saucedemo.com/"
+    When the user enters "standard_user" and "secret_sauce"
+    Then the user should be able to login
+    And the 'Sauce Labs' logo is displayed
+    When the user clicks on 'Add Product button'
+    Then the user closes the browser
+
+  Scenario: Purchase the products and checkout
+    Given the user opens the browser and navigates to the homepage "https://www.saucedemo.com/"
+    When the user enters "standard_user" and "secret_sauce"
+    Then the user should be able to login
+    When the user clicks on 'Add Product button'
+    Then the user click on cart logo
